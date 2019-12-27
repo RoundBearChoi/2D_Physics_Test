94c3c77a225c6cde7f57f644b4cae98d74d2901d
c <roundbeargames@gmail.com>
2019-12-27 18:41:55 +0900

    choi
    choi

---
+++ Force_and_Torque/Assets/AddForceAndTorque.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddForceAndTorque : MonoBehaviour
+{
+    Rigidbody2D rigidbody2d;
+    public float RightForce;
+    public float Torque;
+
+    void Start()
+    {
+        rigidbody2d = this.gameObject.GetComponent<Rigidbody2D>();
+
+        rigidbody2d.AddTorque(Torque);
+        rigidbody2d.AddForce(Vector2.right * RightForce);
+    }
+}
